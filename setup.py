1f81e1ac3723245cad5bfa56700ded4f9451e2e7
Peter Andersson <fshsweden@gmail.com>
2020-10-26 10:42:24 +0100

    Back to development: 0.1.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='pantools',
     packages=find_packages(include=['src']),
-    version='0.1.13',
+    version='0.1.14.dev0',
     author='Peter Andersson',
     author_email='peter.andersson@fsh.se',
     description='PAN Tools library',
